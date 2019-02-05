475be8a506c47af518f40addc022a835ca809eb6
flofloaud1034 <florian.audigue@gmail.com>
2019-02-05 13:46:28 +0100

    Add button to check

--- app/src/main/java/com/androflo/ignorebatteryoptimisationcheckerapp/MainActivity.kt
+++ app/src/main/java/com/androflo/ignorebatteryoptimisationcheckerapp/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.androflo.ignorebatteryoptimisationcheckerapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.github.androflo.ignorebatteryoptimisationchecker.IgnoreBatteryOptimisationChecker
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonCheck: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCheck = findViewById(R.id.buttonCheck)
+
+        buttonCheck.setOnClickListener {
+
+            IgnoreBatteryOptimisationChecker(this)
+                .check()
+
+        }
     }
 }
